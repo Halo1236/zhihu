4e75ac3cadc47566dcf922c719f725f280fafeff
halo <wuyihuangw@gmail.com>
2019-04-09 10:53:45 +0800

    增加ide入口

---
+++ README.md
@@ -0,0 +1,6 @@
+# zhihu-spider
+
+scrapy框架学习入门
+
+https://cuiqingcai.com/3472.html
+https://cuiqingcai.com/4380.html
---
+++ zhihuuser/entrypoint.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+created by Halo 2019/4/9 10:33
+"""
+from scrapy.cmdline import execute
+
+execute(['scrapy', 'crawl', 'zhihu'])
